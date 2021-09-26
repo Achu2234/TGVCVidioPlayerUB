b9ae9e0ea0651d8d4ef5117fbb8e8d4df3ddf372
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-09-26 11:10:51 +0530

    Added Multichat

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     API_ID = int(os.environ.get("API_ID", "0"))
     API_HASH = os.environ.get("API_HASH", "")
-    CHAT_ID = int(os.environ.get("CHAT_ID", "0"))
+    LOG_ID = int(os.environ.get("LOG_ID", "0"))
     SESSION_STRING = os.environ.get("SESSION_STRING", "")
 
 class Database:
